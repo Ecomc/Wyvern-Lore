1070cb4443e71e0210611076ac4b71f92398566d
Terminal_ <90926373+TerminalModding@users.noreply.github.com>
2022-02-16 17:57:02 +0800

    Setted Up the Tabs.

--- src/main/java/net/eco/wyvernlore/item/ModItems.java
+++ src/main/java/net/eco/wyvernlore/item/ModItems.java
@@ -13,9 +13,13 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, WyvernLore.MOD_ID);
 
-
+    //Example
     public static final RegistryObject<Item> TEST_ITEM = ITEMS.register("test_item",
             () -> new Item(new Item.Properties().tab(ItemGroup.TAB_MISC)));
+
+    //Ores
+    public static final RegistryObject<Item> CORRUPTIA = ITEMS.register("corruptia",
+            () -> new Item(new Item.Properties().tab(ModItemsTab.WL_ORES)));
 
 
     public static void register(IEventBus eventBus) {
