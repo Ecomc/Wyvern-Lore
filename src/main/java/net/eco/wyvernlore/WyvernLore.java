0b5c22e589e8320adfab5fa6e4ad012d41c6a9c4
Terminal_ <90926373+TerminalModding@users.noreply.github.com>
2022-02-16 08:50:44 +0800

    Initial commit

---
+++ src/main/java/net/eco/wyvernlore/WyvernLore.java
@@ -0,0 +1,27 @@
+package net.eco.wyvernlore;
+
+import net.eco.wyvernlore.item.ModItems;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import net.minecraftforge.eventbus.api.IEventBus;
+
+
+@Mod(WyvernLore.MOD_ID)
+public class WyvernLore
+{
+
+    public static final String MOD_ID = "wyvernlore";
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public WyvernLore() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+}
